f956acd7431f98426acfaa8c9d58d36075966451
Wang Xin E <xin.e.wang@tieto.com>
2018-05-29 14:42:47 +0200

    added .h

--- learn/learn.pro
+++ learn/learn.pro
@@ -16,3 +16,6 @@ DEFINES += QT_DEPRECATED_WARNINGS
 
 SOURCES += \
         main.cpp
+
+HEADERS += \
+    std_lib_facilities.h
--- learn/main.cpp
+++ learn/main.cpp
@@ -1,6 +1,8 @@
-#include <iostream>
+#include "std_lib_facilities.h"
 
 int main()
 {
 	std::cout << "h." << std::endl;
+	keep_window_open();
+	return 0;
 }
