05b21bb5bfc8ee7c9da1fb33e76d8266f0fcee79
Wang Xin E <xin.e.wang@tieto.com>
2018-06-07 12:31:14 +0200

    Test map and pointer

--- main.cpp
+++ main.cpp
@@ -1,8 +1,30 @@
+#include <iterator>
+#include <map>
 #include "std_lib_facilities.h"
 
 int main()
 {
 	std::cout << "h." << std::endl;
+
+	// test std::map
+
+	std::map<std::string, int> TerminalID;
+	TerminalID.insert(std::make_pair("Tokyo", 1));
+	TerminalID.insert(std::make_pair("Dalian", 2));
+	TerminalID["Barcelona"] = 3;
+	TerminalID["Viena"]     = 4;
+	TerminalID["Barcelona"] += 5;
+	std::map<std::string, int>::iterator findKey;
+	if ((findKey = TerminalID.find("Tokyo")) != TerminalID.end())
+		std::cout << "..." << findKey->first << "::" << findKey->second << std::endl;
+	else
+		std::cout << "not found" << std::endl;
+
+	// test pointer
+	int* iPtr = NULL;
+	int  b    = 90;
+	iPtr      = &b;
+
 	keep_window_open();
 	return 0;
 }
